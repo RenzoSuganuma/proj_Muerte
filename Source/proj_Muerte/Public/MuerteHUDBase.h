e75e9a8100134f00e620b47e34f61c0282248428
RENZO SUGANUMA <discoverygworks2023@gmail.com>
2025-09-17 05:56:53 +0900

    [add] add BP

---
+++ Source/proj_Muerte/Public/MuerteHUDBase.h
@@ -0,0 +1,17 @@
+// Copyright (c) 2025, DiscoveryGameWorks All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "MuerteHUDBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJ_MUERTE_API AMuerteHUDBase : public AHUD
+{
+	GENERATED_BODY()
+	
+};
