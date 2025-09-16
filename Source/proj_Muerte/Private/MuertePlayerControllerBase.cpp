c8ec0847ddae4f2c2249bba2647a9352627a707d
RENZO SUGANUMA <discoverygworks2023@gmail.com>
2025-09-17 07:55:45 +0900

    no message

--- Source/proj_Muerte/Private/MuertePlayerControllerBase.cpp
+++ Source/proj_Muerte/Private/MuertePlayerControllerBase.cpp
@@ -20,20 +20,12 @@ void AMuertePlayerControllerBase::BeginPlay()
 		component->BindAction(m_inputActionMove, ETriggerEvent::Triggered, this,
 		                      &AMuertePlayerControllerBase::ActionMove);
 	}
-	else if (!IsValid(component))
-	{
-		UKismetSystemLibrary::PrintString(this, TEXT("Move Bind Failure"));
-	}
 
 	if (UEnhancedInputLocalPlayerSubsystem* inputSystem = GetLocalPlayer()->GetSubsystem<
 		UEnhancedInputLocalPlayerSubsystem>())
 	{
 		inputSystem->ClearAllMappings();
 		inputSystem->AddMappingContext(m_mappingContext, 0);
-	}
-	else if (!IsValid(inputSystem))
-	{
-		UKismetSystemLibrary::PrintString(this, TEXT("Map Bind Failure"));
 	}
 }
 
